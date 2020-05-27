7c8c34a5c2c25cfb265e36f529fd3d04d9a6095f
Kaan Karakaya <yusufkaan142@gmail.com>
2020-05-28 01:28:16 +0300

    Add no sandbox for docker
    Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('https://www.mgm.gov.tr/tahmin/il-ve-ilceler.aspx?il=Elazig');
   
